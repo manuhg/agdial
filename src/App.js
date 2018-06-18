34b8986aff398e1700788775948e5b6976a035fe
manu-spherical <manuhegdev@gmail.com>
2018-06-18 23:12:33 +0530

    setup a default route, added empty components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logo from 'resources/logo.png';
+import 'resources/App.css';
+import { Link } from 'react-router-dom';
 class App extends Component {
-  render() {
+    render() {
+        const { location } = this.props;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" /> </Link>
+          <h1 className="App-title">Welcome to AgDial</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>{location.pathname}</code>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'resources/index.css';
+import App from 'App';
 // import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+    <Switch>
+    <Route path="/"  component={App} />
+    </Switch>
+    </Router>
+    , document.getElementById('root'));
 // registerServiceWorker();
