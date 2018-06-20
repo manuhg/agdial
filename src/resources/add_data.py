0dc5249227e7d19955e276f5878978f2fb922ae3
manu-spherical <manuhegdev@gmail.com>
2018-06-20 17:24:58 +0530

    added categories

--- src/resources/add_data.py
+++ src/resources/add_data.py
@@ -4,17 +4,29 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from parse import xml_to_list
 
-lst=xml_to_list('categories.xml')
+def print_collection(db,collection):
+    print("Collection:",collection)
+    ref = db.collection(collection)
+    docs = ref.get()
+    list(map(lambda d:print(d.id,'\n',d.to_dict()),docs))
 
-cred = credentials.Certificate(os.environ['HOME']+"/agdial-001-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+def init_db():
+    cred = credentials.Certificate(os.environ['HOME']+"/agdial-001-firebase-adminsdk.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
 
-
-def read_collection(db,collection):
-	ref = db.collection(collection)
-	docs = ref.get()
-	for doc in docs:
-	    print(doc.id, doc.to_dict())
+from parse import xml_to_dict
+def add_categories():
+    cat_dict=xml_to_dict('categories.xml')
+    if cat_dict:
+        print('Categories:\n',cat_dict)
+        db=init_db()
+        coll_name='categories'
+        #print(db.collection(coll_name).document(coll_name).set({'c1':list(map(lambda x:list(x.keys())[0],cats_obj)),'vals':cats_obj}))
+        doc_ref=db.collection(coll_name).document(coll_name)
+        print(doc_ref.set({coll_name:cat_dict}))
+def main():
+    add_categories()
+main()
--- src/resources/parse.py
+++ src/resources/parse.py
@@ -2,7 +2,7 @@
 #FIRST remome all <row> s
 
 import xml.etree.ElementTree as et
-#from functools import reduce
+from functools import reduce
 
 def pc(s):
 	#PascalCase
@@ -10,10 +10,12 @@ def pc(s):
 	return ' '.join(list(map(lambda w:w[0].upper()+w[1:],s.split(' ')))).strip()
 	#return reduce(lambda s,x:(s+' '+x).strip(),list(map(lambda w:w[0].upper()+w[1:],s.split(' '))))
 
+#def entry(e):
+#    	return list([pc(e[0]),list(map(lambda li:pc(li[0]),e[1]))])
 def entry(e):
-	return list([pc(e[0]),list(map(lambda li:pc(li[0]),e[1]))])
+    	return (pc(e[0]),list(map(lambda li:pc(li[0]),e[1])))
 
-def xml_to_list(xml_file):
+def xml_to_dict(xml_file):
     tree=et.parse(xml_file)
     root=tree.getroot()
-    return list(map(entry,root[0][0][0]))
+    return dict(list(map(entry,root[0][0][0])))
