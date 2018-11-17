932ef49849fddfbd11b0d26a448169a085568198
manu-spherical <manuhegdev@gmail.com>
2018-11-17 20:03:11 +0530

    contactus fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
             <Route path="/about" component={About} />
             <Route path="/pricing" component={Pricing} />
             {/* <Route path="/joinus" component={Join} /> */}
-            <Route path="/contact" component={Contact} />
+            {/* <Route path="/contact" component={Contact} /> */}
             <Route path="/search" component={Search} />
 
             {/* <Route component={Home} _404={true} /> */}
